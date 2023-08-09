e47083a8ebba93545f649bb2eab5348c91cbc2cf
Alex <aironer@yandex.ru>
2023-08-09 17:22:19 +0300

    Комментарии по реализации ProgressBar и VolumeBar 1. Как обрабатывать / подписываться на события в блоке аудио 2. Логика переключения продолжительности времени трека 3. Работа с состоянием

--- src/components/ProgressBar/progressBar.jsx
+++ src/components/ProgressBar/progressBar.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import * as S from './progressBar.styled'
 
 export default function ProgressBar({ audioRef }) {
-  const [value, setValue] = useState('1')
+  const [сurrentTime, setCurrentTime] = useState('1')
 
   const [time, setTime] = useState('0') // записываем время
 
@@ -17,17 +17,31 @@ export default function ProgressBar({ audioRef }) {
     audioRef.current.currentTime = event.target.value // меняем время трека в зав-ти от того где нах-ся ползунок/
   }
   useEffect(() => {
+    // срабатывает при отрисовке компонента ProgressBar
+    // Если audioRef?.current.duration - существует, то duration - максимальное время(или продолжительность трека) записывается в setTime -> time
     if (audioRef?.current.duration) {
+      // Если audioRef изменяется (переключается трек), меняем setTime -> Time
       setTime(audioRef.current.duration)
     }
+    // затем передаем значение продолжительности трека в input: max={time} (нах-ся в return)
   })
 
   useEffect(() => {
+    // у блока audio есть свои события (event), в докум-ии
+    // Событие timeupdate происходит при изменении игрового положения аудио/видео.
+    // Это событие вызывается:
+    // При перемещении позиции воспроизведения (например, когда пользователь пересылается в другую точку аудио/видео)
     audioRef.current.addEventListener('timeupdate', () => {
-      setValue(audioRef.current.currentTime)
+      // подписываемся на изменения timeupdate (изменение времени в блоке <audio>)
+      // Как только будет изменятся текущее время трека - будет срабатывать эта логика:
+      setCurrentTime(audioRef.current.currentTime)
+      // записываем текущее время в блоке аудио в переменную currentTime (которая отвечает за текущую позицию в прогресс баре)
+      // таким образом ползунок в прогресс баре двигается, перезаписывается переменная currentTime, значение которой стан-ся больше и больше, из за этого наглядно видим изменения/движение ползунка
       return () => {
+        // нужно отписаться от обработчика event-a (когда блок пропал, т.е. при переключении трека нужно отказаться от текущего обработчика)
+        // при след треке обработчик навесится заново
         audioRef.current.removeEventListener('timeupdate', () => {
-          setValue(audioRef.current.currentTime)
+          setCurrentTime(audioRef.current.currentTime)
         })
       }
     })
@@ -40,12 +54,12 @@ export default function ProgressBar({ audioRef }) {
         name="range"
         min="0"
         max={time}
-        value={value}
+        сurrentTime={сurrentTime}
         step="0.01"
         onInput={(event) => changeProgress(event)}
       />
       <S.BarTime>
-        {formatTime(Math.floor(value))}/
+        {formatTime(Math.floor(сurrentTime))}/
         {formatTime(Math.floor(audioRef.current?.duration))}
       </S.BarTime>
     </>
--- src/components/VolumeBar/VolumeBar.jsx
+++ src/components/VolumeBar/VolumeBar.jsx
@@ -7,9 +7,18 @@ export default function VolumeBar({ audioRef }) {
   const [value, setValue] = useState('1') // value - пок-т какой ур-нь громкости
 
   const changeValue = (event) => {
-    setValue(event.target.value)
+    // принимаем event
+    setValue(event.target.value) // передаем значение event в setValue -> value
+    // event содержит в себе текущую инф-ю на каком этапе находится value от 0 до 1
 
+    // audioRef принимаем в арг-те ф-ии volumeBar чтобы далее получить доступ к <audio>
+
+    // У нас есть аудио с которым мы должны взаимодействовать, и у него изменить значение громкости, именно на то value, которое у нас на 34 строке: value={value} (которое мы изменяем)
+
+    // принимаем ref от аудио - audioRef. Получаем так доступ к аудио.
+    // обращаемся к св-ву обьекта - громкости
     audioRef.current.volume = parseFloat(value)
+    // в перем-ую volume передали значение value нашего бара. Теперь будет меняться громкость в зависимости от положения
   }
   return (
     <S.BarVolumeBlock>
@@ -27,7 +36,7 @@ export default function VolumeBar({ audioRef }) {
             max="1"
             value={value}
             step="0.01"
-            onInput={(event) => changeValue(event)}
+            onInput={(event) => changeValue(event)} // передаем event в ф-ию
           />
         </S.VolumeProgress>
       </S.VolumeContent>
